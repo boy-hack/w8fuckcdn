3851ae17b623e9ea67a070efc3a3494486b2b3ad
boyhack <34109680@qq.com>
2018-08-24 21:33:50 +0800

    debug&update

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 #author:w8ay
 
-host = "bbs.125.la" # 需要查找网站的域名
+host = "www.hacking8.com" # 需要查找网站的域名
 
-keyword = "Discuz! X3.2" # 需要查找的关键字 结果没有解码，最好是英文
+keyword = "Hello World" # 需要查找的关键字 结果没有解码，最好是英文
 
 timeout = 15 # 连接网站时超时IP
 
-HTTPS_Support = True # 此参数为True时，将进行HTTPS访问，masscan将扫描443端口，并且扫描https的网址
+HTTPS_Support = False # 此参数为True时，将进行HTTPS访问，masscan将扫描443端口，并且扫描https的网址
